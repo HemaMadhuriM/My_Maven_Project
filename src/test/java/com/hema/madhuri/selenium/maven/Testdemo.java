47482a477a1522e7e043ee51c44566723fe30ce3
HemaMadhuriM <hemu.madhuri@gmail.com>
2021-06-09 21:19:11 +0530

    Added My Maven Project

---
+++ src/test/java/com/hema/madhuri/selenium/maven/Testdemo.java
@@ -0,0 +1,26 @@
+package com.hema.madhuri.selenium.maven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Testdemo {
+	@Test
+public static void demo() throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		System.out.println("chrome Launched successfully");
+		driver.manage().window().maximize();
+		System.out.println("Window MAximized sucesfully");
+		driver.get("http://www.gmail.com");
+		System.out.println("opened the url"+driver.getCurrentUrl());
+		
+		
+		Thread.sleep(5000);
+		driver.close();
+
+	}
+}
